97298b194a15608109e603b0e3bdb8d6ed1c5958
Rui Armada <rarmada@ext.worten.pt>
2023-03-30 18:35:54 +0100

    main

--- main.tf
+++ main.tf
@@ -1,21 +1,17 @@
-# You MUST have an EKS cluster already existing to run ingress Nginx on it
+
 module "public_eks_cluster" {
     
   source         = "github.com/rui-armada/terraform-modules/aws-eks"
-  vpc_name       = "my-VPC1"
-  cluster_name   = "my-EKS1"
+  vpc_name       = "my-VPC"
+  cluster_name   = "my-EKS"
   desired_size   = 2
   max_size       = 2
   min_size       = 1
   instance_types = ["t3.medium"]
 }
-module "ingress-nginx" {
-  # note update the source link with the required version
+
+module "lb-nginx" {
+
   source     = "github.com/rui-armada/terraform-modules/helm-lb"
   cluster_id = module.public_eks_cluster.id
-
-
-}
-output "k8s_service_ingress_elb" {
-  value=module.ingress-nginx.k8s_service_ingress_elb
 }
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "k8s_service_ingress_elb" {
+  value=module.ingress-nginx.k8s_service_ingress_elb
+}
